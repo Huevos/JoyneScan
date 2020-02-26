ac6f6a57d2ea327c49fd238f9331ffd129cf9963
Huevos <rowland@moralet.com>
2020-02-26 18:51:43 +0100

    Spelling error.

--- JoyneScan/src/joynescan.py
+++ JoyneScan/src/joynescan.py
@@ -159,7 +159,7 @@ class JoyneScan(Screen): # the downloader
 
 		if len(self.actionsList) > self.index and self.actionsList[self.index] == "read NIT":
 			if not inStandby:
-				self["status"].setText(_("Seraching for Joyne transponders..."))
+				self["status"].setText(_("Searching for Joyne transponders..."))
 			self.transpondercurrent = self.homeTransponder
 			
 			self.timer = eTimer()
