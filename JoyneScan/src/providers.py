903a0925f8b993c963f01d667f4c9152e55e073b
Huevos <rowland@moralet.com>
2020-03-17 21:07:17 +0100

    Experimental addition of an "M7" provider

--- JoyneScan/src/providers.py
+++ JoyneScan/src/providers.py
@@ -32,4 +32,20 @@ PROVIDERS = {
 			"original_network_id": 0x009e,
 			"transport_stream_id": 0xc364,},
 		"bat": {
-			"BouquetID": 0x1,},},}
+			"BouquetID": 0x1,},},
+	"Canal_Digitaal_HD": {
+		"name": _("Canal Digitaal HD"), 
+		"transponder": {
+			"frequency": 12515000,
+			"symbol_rate": 22000000,
+			"polarization": eDVBFrontendParametersSatellite.Polarisation_Horizontal,
+			"fec_inner": eDVBFrontendParametersSatellite.FEC_5_6,
+			"orbital_position": 192,
+			"system": eDVBFrontendParametersSatellite.System_DVB_S,
+			"modulation": eDVBFrontendParametersSatellite.Modulation_QPSK,
+			"roll_off": eDVBFrontendParametersSatellite.RollOff_alpha_0_35,
+			"original_network_id": 0x0035,
+			"transport_stream_id": 0x0451,
+			"nit_pid": 0x385,
+			"nit_current_table_id": 0xbc,
+			"nit_other_table_id": 0x00,},},}
