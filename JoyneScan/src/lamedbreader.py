b52b36375be66f03d7c50dab2d959f1ca230e656
Twol <tonywhalley@t-online.de>
2021-05-10 14:42:42 +0200

    lamedbreader - use floor division

--- JoyneScan/src/lamedbreader.py
+++ JoyneScan/src/lamedbreader.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from __future__ import division
 import re
 
 class LamedbReader():
@@ -126,7 +127,7 @@ class LamedbReader():
 
 		srv_blocks = content[srv_start + 9:srv_stop].strip().split("\n")
 
-		for i in range(0, len(srv_blocks)/3):
+		for i in range(0, len(srv_blocks)//3):
 			service_reference = srv_blocks[i*3].strip()
 			service_name = srv_blocks[(i*3)+1].strip()
 			service_provider = srv_blocks[(i*3)+2].strip()
